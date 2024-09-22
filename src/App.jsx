6cc785098dc5a040071ebeaa7677e5eeece224c4
BALAJINR2023 <ravibalajinr@gmail.com>
2024-09-22 07:41:55 +0530

    nill

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import AppRoutes from './Layout/Approutes'
-import { CarFilterProvider } from './Pages/CarFilterContext'
 
 function App() {
    return (
     <>
-    <CarFilterProvider>
     <AppRoutes/>
-    </CarFilterProvider>
     </>
   ) 
 }
